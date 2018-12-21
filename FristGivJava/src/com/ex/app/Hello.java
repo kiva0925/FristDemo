90c08453b7f5e6f2ca9926292759ae6359504050
DESKTOP-3CH7IN9\12540 <lucenhao0925@gmail.com>
2018-12-21 20:12:01 +0800

    添加hello.java文件

---
+++ FristGivJava/src/com/ex/app/Hello.java
@@ -0,0 +1,9 @@
+package com.ex.app;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.print("hello");
+        System.out.print("\t");
+        System.out.print("world");
+    }
+}
